ed3fe7761892255e42239a8238915146e5e21ea5
skh-2024 <s.k.honkanen@gmail.com>
2025-01-12 16:44:20 +0100

    Add files via upload

---
+++ Arbeidskrav_2/Oppgave_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 12 16:05:40 2025
+
+PY1010_Arbeidskrav_2_oppgave_1
+Beregne alder i 2024
+
+@author: s.honkanen
+"""
+
+# Programmet spør brukeren om fødselsåret (input)
+alder = int(input('Hvilket år er du født? '))
+
+# Programmet regner ut hvor gammel personen blir i løpet av år 2024
+alder = 2024 - alder
+
+# Programmet skriver ut resultatet med passende tekst
+print(f"Du blir {alder} år gammel i løpet av året 2024.")
